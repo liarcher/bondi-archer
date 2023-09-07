b0e8576481ff7f4a480816cdec95a18d4bca84ac
Eric Cha <z5324960@ad.unsw.edu.au>
2023-09-07 19:51:01 +1000

    changes

--- app2.py
+++ app2.py
@@ -3,13 +3,24 @@ import pandas as pd
 
 app = Flask(__name__)
 
-# 读取Excel文件
-excel_file = 'data.xlsx'  # 替换为您的Excel文件路径
+excel_file = 'data.xlsx'  #Data input
+
 df = pd.read_excel(excel_file)
 
 @app.route('/')
 def index():
     return render_template('index2.html')
+
+def avg(column):
+    return sum(column) / len(column)
+
+
+def ratio(df):
+    return avg(df.loc['gp']), avg(df.loc['ep']), avg(df.loc['specialist'])
+
+
+def prediction(population, growth, ratio):
+    return round(population * (1 + growth) * ratio)
 
 @app.route('/fetch_data', methods=['POST'])
 def fetch_data():
@@ -18,7 +29,18 @@ def fetch_data():
         data = df[location].to_dict()
         labels = list(data.keys())
         values = list(data.values())
-        return jsonify({'labels': labels, 'values': values})
+
+        expected_gp =10  # Your logic for expected gp
+        expected_ep = 11  # Your logic for expected ep
+        expected_sp = 12
+        return jsonify({
+            'labels': labels, 
+            'values': values,
+            'expected_gp': expected_gp,
+            'expected_ep': expected_ep,
+            'expected_sp': expected_sp
+        })
+        #return jsonify({'labels': labels, 'values': values})
     else:
         return jsonify({'error': 'Location not found'})
 
--- data.py
+++ data.py
@@ -10,42 +10,11 @@ data = {
     'Epping':       [4/20, 22/200, 5/20, 20, 0.022],
     'Blacktown':    [1/11, 9/11, 4/11, 11, 0.022],
     'Randwick':     [2/10, 3/10, 6/10, 10, 0.000],
-    'Average' :     [0.199990836, 0.225430108, 0.290411168, 17.125, 0.0285]
+    'Average' :     [0.199990836, 0.225430108, 0.290411168, 17.125, 0.0185]
 }
 
 # Create a dataframe
 df = pd.DataFrame(data, index=['gp', 'ep', 'specialist', 'population(k)', 'population growth'])
-
-
-def avg(column):
-    return sum(column) / len(column)
-
-
-def ratio(df):
-    return avg(df.loc['gp']), avg(df.loc['ep']), avg(df.loc['specialist'])
-
-
-def prediction(population, growth, ratio):
-    return round(population * (1 + growth) * ratio)
-
-
-# Create a dataframe
-city = 'Kingsford'
-data = pd.DataFrame(data, index=['gp', 'ep', 'specialist', 'population(k)', 'population growth'], columns=[city])
-# print(data)
-gp_ratio, ep_ratio, specialist_ratio = ratio(data)
-print(gp_ratio, ep_ratio, specialist_ratio)
-city = data[city]
-# print(city)
-city.loc['expected_gp'] = prediction(city.loc['population(k)'], city.loc['population growth'], gp_ratio)
-city.loc['expected_ep'] = prediction(city.loc['population(k)'], city.loc['population growth'], ep_ratio)
-city.loc['expected_sp'] = prediction(city.loc['population(k)'], city.loc['population growth'], specialist_ratio)
-print(city)
-
-# Save dataframe to an Excel file
-city.to_excel(f'{city}_data.xlsx')
-
-print(f"Excel file '{city}_data.xlsx' has been created!")
 
 # Save dataframe to an Excel file
 df.to_excel('data.xlsx')
